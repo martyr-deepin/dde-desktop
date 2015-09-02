759945cf2232a6fe366a6059bc692e62f2f24a50
dragondjf <ding465398889@163.com>
2015-09-02 20:21:36 +0800

    fix desktop flags settings
    Change-Id: I3fc5346b55b3f62006b337fdd05e7179dd27bbba

--- views/translucentframe.cpp
+++ views/translucentframe.cpp
@@ -5,8 +5,6 @@ TranslucentFrame::TranslucentFrame(QWidget *parent) : QFrame(parent)
 {
     setAttribute(Qt::WA_TranslucentBackground);
     setAttribute(Qt::WA_DeleteOnClose);
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnBottomHint);
-    setWindowState(Qt::WindowMaximized);
     setGeometry(qApp->desktop()->availableGeometry());
     XcbMisc::instance()->set_window_type(winId(),
                                          XcbMisc::Desktop);
