3d44185e38c1a688597a787a333f2c4c000d428b
Hualet Wang <mr.asianwang@gmail.com>
2016-01-04 17:22:51 +0800

    update source ts
    Change-Id: I4e7653c99b496cc537e5d519995f883324cc76fa

--- translations/dde-desktop.ts
+++ translations/dde-desktop.ts
@@ -45,12 +45,12 @@
 <context>
     <name>DTaskDialog</name>
     <message>
-        <location filename="../dialogs/dtaskdialog.cpp" line="356"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="355"/>
         <source>There is %1 task in progress</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialogs/dtaskdialog.cpp" line="358"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="357"/>
         <source>There are %1 tasks in progress</source>
         <translation type="unfinished"></translation>
     </message>
@@ -58,13 +58,23 @@
 <context>
     <name>DesktopApp</name>
     <message>
-        <location filename="../app/desktopapp.cpp" line="85"/>
+        <location filename="../app/desktopapp.cpp" line="111"/>
         <source>Are you sure to delete %1 ?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/desktopapp.cpp" line="87"/>
+        <location filename="../app/desktopapp.cpp" line="113"/>
         <source>Are you sure to delete these %1 items?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/desktopapp.cpp" line="149"/>
+        <source>&quot;%1&quot; already exists, please select a different name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/desktopapp.cpp" line="150"/>
+        <source>Confirm</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -72,12 +82,36 @@
     <name>DesktopItemManager</name>
     <message>
         <location filename="../views/desktopitemmanager.cpp" line="30"/>
+        <location filename="../views/desktopitemmanager.cpp" line="31"/>
         <source>Computer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/desktopitemmanager.cpp" line="58"/>
+        <location filename="../views/desktopitemmanager.cpp" line="65"/>
         <source>Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MenuController</name>
+    <message>
+        <location filename="../controllers/menucontroller.cpp" line="166"/>
+        <source>Cut(_X)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controllers/menucontroller.cpp" line="167"/>
+        <source>Copy(_C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controllers/menucontroller.cpp" line="168"/>
+        <source>Paste(_V)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controllers/menucontroller.cpp" line="169"/>
+        <source>Select All(_A)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -109,21 +143,35 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../dialogs/dtaskdialog.cpp" line="155"/>
+        <source>copyed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../dialogs/dtaskdialog.cpp" line="156"/>
-        <location filename="../dialogs/dtaskdialog.cpp" line="164"/>
+        <source>&lt;span style=&quot;color: #3cadff&quot;&gt; %1 &lt;/span&gt; is %2 to &lt;span style=&quot;color: #3cadff&quot;&gt; %3 &lt;/span&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/dtaskdialog.cpp" line="162"/>
+        <source>moved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/dtaskdialog.cpp" line="163"/>
         <source>&lt;span style=&quot;color: #3cadff&quot;&gt; %1 &lt;/span&gt;                                  is %2 to &lt;span style=&quot;color: #3cadff&quot;&gt; %3 &lt;/span&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialogs/dtaskdialog.cpp" line="159"/>
-        <location filename="../dialogs/dtaskdialog.cpp" line="168"/>
-        <location filename="../dialogs/dtaskdialog.cpp" line="173"/>
-        <location filename="../dialogs/dtaskdialog.cpp" line="183"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="158"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="167"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="172"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="182"/>
         <source>current speed:%1 time Left:%2 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialogs/dtaskdialog.cpp" line="171"/>
+        <location filename="../dialogs/dtaskdialog.cpp" line="170"/>
         <source>&lt;span style=&quot;color: #3cadff&quot;&gt; %1 &lt;/span&gt;                                  is deleted </source>
         <translation type="unfinished"></translation>
     </message>
