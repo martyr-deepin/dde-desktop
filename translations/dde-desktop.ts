203a0111fddaf63013fa7baf4fecfd197fc2fddc
dragondjf <ding465398889@163.com>
2016-01-21 10:00:32 +0800

    fix copyed to copied
    Change-Id: I568321f20d286978b0ecbf144da5d038a88f12b1

--- translations/dde-desktop.ts
+++ translations/dde-desktop.ts
@@ -58,22 +58,22 @@
 <context>
     <name>DesktopApp</name>
     <message>
-        <location filename="../app/desktopapp.cpp" line="111"/>
+        <location filename="../app/desktopapp.cpp" line="114"/>
         <source>Are you sure to delete %1 ?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/desktopapp.cpp" line="113"/>
+        <location filename="../app/desktopapp.cpp" line="116"/>
         <source>Are you sure to delete these %1 items?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/desktopapp.cpp" line="149"/>
+        <location filename="../app/desktopapp.cpp" line="152"/>
         <source>&quot;%1&quot; already exists, please select a different name.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/desktopapp.cpp" line="150"/>
+        <location filename="../app/desktopapp.cpp" line="153"/>
         <source>Confirm</source>
         <translation type="unfinished"></translation>
     </message>
@@ -95,22 +95,22 @@
 <context>
     <name>MenuController</name>
     <message>
-        <location filename="../controllers/menucontroller.cpp" line="166"/>
+        <location filename="../controllers/menucontroller.cpp" line="167"/>
         <source>Cut(_X)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controllers/menucontroller.cpp" line="167"/>
+        <location filename="../controllers/menucontroller.cpp" line="168"/>
         <source>Copy(_C)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controllers/menucontroller.cpp" line="168"/>
+        <location filename="../controllers/menucontroller.cpp" line="169"/>
         <source>Paste(_V)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controllers/menucontroller.cpp" line="169"/>
+        <location filename="../controllers/menucontroller.cpp" line="170"/>
         <source>Select All(_A)</source>
         <translation type="unfinished"></translation>
     </message>
@@ -143,11 +143,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialogs/dtaskdialog.cpp" line="155"/>
-        <source>copyed</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../dialogs/dtaskdialog.cpp" line="156"/>
         <source>&lt;span style=&quot;color: #3cadff&quot;&gt; %1 &lt;/span&gt; is %2 to &lt;span style=&quot;color: #3cadff&quot;&gt; %3 &lt;/span&gt;</source>
         <translation type="unfinished"></translation>
@@ -168,6 +163,11 @@
         <location filename="../dialogs/dtaskdialog.cpp" line="172"/>
         <location filename="../dialogs/dtaskdialog.cpp" line="182"/>
         <source>current speed:%1 time Left:%2 </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/dtaskdialog.cpp" line="155"/>
+        <source>copied</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
