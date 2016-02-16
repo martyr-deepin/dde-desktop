1c2b5b99577febd65df3a0bb2da5bcfce193c1d0
Xu Fasheng <fasheng.xu@gmail.com>
2016-02-16 13:24:22 +0800

    add copyright for source files
    Change-Id: I38e7e1629469fc4be463e8772c868c51470da18f

--- widgets/commandlinemanager.cpp
+++ widgets/commandlinemanager.cpp
@@ -1,4 +1,13 @@
-﻿#include "commandlinemanager.h"
+/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/
+
+#include "commandlinemanager.h"
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 #include <QCoreApplication>
